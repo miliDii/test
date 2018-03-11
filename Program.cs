99e1758dafc95356ecc06d3337d740925973e5a0
mila <80.mila@gmail.com>
2018-03-11 10:38:34 +0200

    delete and add

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace max_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine(GetMax(GetMax(num1,num2),num3));
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+           
+            if (num1 > num2)
+                return num1;
+            else
+                return num2;
+
+
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # test
 how its work
 add homework
+delete file
+add program
